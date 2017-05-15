300ce20413c14ca4c9c11a74bf396aceccd13897
Dell'Aversana <ilariada95@gmail.com>
2017-05-15 12:35:53 +0200

    Prenota2

---
+++ app/src/main/java/com/example/lucia/applicazionelab/MainETab/prenota2.java
@@ -0,0 +1,15 @@
+package com.example.lucia.applicazionelab.MainETab;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.lucia.applicazionelab.R;
+
+public class prenota2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prenota2);
+    }
+}
