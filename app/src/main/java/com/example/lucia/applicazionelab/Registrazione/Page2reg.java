400001995a0454a5c0f3b2beed9ecaf9aabba02b
Luzix3 <luciamigliaccio3@gmail.com>
2017-05-16 20:21:27 +0200

    modifiche varie

--- app/src/main/java/com/example/lucia/applicazionelab/Registrazione/Page2reg.java
+++ app/src/main/java/com/example/lucia/applicazionelab/Registrazione/Page2reg.java
@@ -1,11 +1,13 @@
 package com.example.lucia.applicazionelab.Registrazione;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 
 import com.example.lucia.applicazionelab.R;
@@ -19,6 +21,9 @@ public class Page2reg extends Activity {
     Button btnReg;
     EditText Editemail;
     EditText Editpass;
+    CheckBox Cutente;
+    CheckBox Cgestore;
+    CheckBox Ccondizioni;
 
 
     @Override
@@ -30,6 +35,9 @@ public class Page2reg extends Activity {
         btnReg = (Button)findViewById(R.id.buttonAvanti);
         Editemail = (EditText)findViewById(R.id.editRegEmail);
         Editpass = (EditText)findViewById(R.id.editRegPass);
+        Cutente = (CheckBox)findViewById(R.id.checkBoxCliente);
+        Cgestore = (CheckBox)findViewById(R.id.checkBoxGestore);
+        Ccondizioni= (CheckBox)findViewById(R.id.checkBoxCondizioni);
 
         btnReg.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,24 +55,37 @@ public class Page2reg extends Activity {
                     Editpass.setError(getString(R.string.obbligatorio));
                 }
 
-                // todo: mettere caratteri obbligatori alla password e mettere emailvalida
-                if(!email2.isEmpty() && !password2.isEmpty())
-                {
-                    final FirebaseAuth mAuth2 = FirebaseAuth.getInstance();
-                    mAuth2.createUserWithEmailAndPassword(email2,password2)
-                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                                @Override
-                                public void onComplete(@NonNull Task<AuthResult> task) {
 
-                                }
-                            });
+
+                if (Cgestore.isChecked())
+                {
+                    Cgestore.setError(getString(R.string.errorecasella));
                 }
 
+                if (!(Cutente.isChecked()) )
+                {
+                    Cutente.setError(getString(R.string.obbligatorio));
+                }
 
+                if (!(Ccondizioni.isChecked()) )
+                {
+                    Ccondizioni.setError(getString(R.string.obbligatorio));
+                }
+                // todo: mettere caratteri obbligatori alla password e mettere emailvalida
 
+                if(!email2.isEmpty() && !password2.isEmpty())
+                {
+                    // todo: creare oggetto daabase
+                    //problema qui!!
+                    Intent openPageRegSucc = new Intent(Page2reg.this, RegSuccess.class);
+                    startActivity(openPageRegSucc);
+                }
 
              }
         });
+
+
+
 
     }
 
---
+++ app/src/main/java/com/example/lucia/applicazionelab/Registrazione/RegSuccess.java
@@ -0,0 +1,15 @@
+package com.example.lucia.applicazionelab.Registrazione;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.lucia.applicazionelab.R;
+
+public class RegSuccess extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reg_success);
+    }
+}
